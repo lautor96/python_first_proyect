d0ef6d7bf1daf72d4057d0005d36e5b144f56b11
Laura Torres Ochoa <92593998+lautor96@users.noreply.github.com>
2021-11-09 10:49:32 -0500

    Print in Python

---
+++ main.py
@@ -0,0 +1,14 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+mensaje="Hola chicos"
+integer_var=123
+float_var=123.24
+
+print(mensaje)
+print(type(mensaje))
+
+print(integer_var)
+print(type(integer_var))
+
+print(float_var)
+print(type(float_var))
