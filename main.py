e33dd54e12cb68ad597b5de8662415996584d631
Laura Torres Ochoa <92593998+lautor96@users.noreply.github.com>
2021-11-09 11:08:09 -0500

    Update main.py

--- main.py
+++ main.py
@@ -12,3 +12,18 @@ print(type(integer_var))
 
 print(float_var)
 print(type(float_var))
+
+integer_1=50
+integer_2=100
+integer_total=integer_1+integer_2
+print(integer_total)
+
+float_1=55.6
+float_2=22.3
+float_total=float_1+float_2
+print(float_total)
+
+mensaje_1="Mi nombre es: "
+mensaje_2="Laura"
+mensaje_completo= mensaje_1 + mensaje_2
+print(mensaje_completo)
